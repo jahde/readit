78b22ee327966dc0ab6fb2ff34db2c497853d86d
Jahde <jahde@me.com>
2015-02-23 21:32:14 -0500

    Added and setup acts_as_votable

--- Gemfile
+++ Gemfile
@@ -9,6 +9,12 @@ gem 'sqlite3'
 # Add users
 gem 'devise', '~>3.3.0'
 
+# Add bootstrap
+gem 'bootstrap-sass', '~> 3.2.0.2'
+
+# Votable
+gem 'acts_as_votable', '~> 0.10.0'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Readit::Application.routes.draw do
   devise_for :users
-  resources :links
+  resources :links do
+    put "like", to: "links#upvote"
+    put "dislike", to: "links#downvote"
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
